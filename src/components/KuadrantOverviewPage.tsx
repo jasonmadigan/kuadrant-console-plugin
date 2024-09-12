fffedbb26d46948d451eff0fab1620b6b980ffb7
Jason Madigan <jason@jasonmadigan.com>
2024-09-12 17:49:30 +0100

    resource links
    Signed-off-by: Jason Madigan <jason@jasonmadigan.com>

--- src/components/KuadrantOverviewPage.tsx
+++ src/components/KuadrantOverviewPage.tsx
@@ -6,12 +6,15 @@ import {
   Page,
   PageSection,
   Title,
+  Card,
+  CardTitle,
+  CardBody,
+  Text
 } from '@patternfly/react-core';
 import { Divider, Flex, FlexItem } from '@patternfly/react-core';
 import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk';
 import './kuadrant.css';
-import { GlobeIcon, MagicIcon, BoxesIcon } from '@patternfly/react-icons';
-
+import { GlobeIcon, ReplicatorIcon, OptimizeIcon, ArrowRightIcon } from '@patternfly/react-icons';
 
 const KuadrantOverviewPage: React.FC = () => {
   const { t } = useTranslation('plugin__console-plugin-template');
@@ -33,26 +36,43 @@ const KuadrantOverviewPage: React.FC = () => {
       <Page>
         <PageSection variant="light">
           <Title headingLevel="h1">{t('Kuadrant')} Overview</Title>
-          <br/>
-          <Flex>
-            <FlexItem flex={{ default: 'flex_1' }}>
-              <Title headingLevel="h4" className="kuadrant-dashboard-learning"><GlobeIcon /> Learning Resources</Title>
-              <p>Learn how to create, import and use {t('Kuadrant')} policies on OpenShift with step-by-step instructions.</p>
-            </FlexItem>
-            <Divider orientation={{ default: 'vertical' }} />
-            <FlexItem flex={{ default: 'flex_1' }}>
-              <Title headingLevel="h4" className="kuadrant-dashboard-feature-highlights"><MagicIcon /> Feature Highlights</Title>
-              <p>Read about the latest information and key features in the Connectivity Link highlights.</p>
-            </FlexItem>
-            <Divider orientation={{ default: 'vertical' }} />
-            <FlexItem flex={{ default: 'flex_1' }}>
-              <Title headingLevel="h4" className="kuadrant-dashboard-enhance"><BoxesIcon /> Enhance Your Work</Title>
-              <p>Ease operational complexity with API management and App Connectivity by using additional Operators and tools.</p>
-            </FlexItem>
-          </Flex>
-          <Divider />
+          <br />
+          <Card>
+            <CardTitle><Title headingLevel="h2">Getting started resources</Title></CardTitle>
+            <CardBody>
+              <Flex>
+                <FlexItem flex={{ default: 'flex_1' }}>
+                  <Title headingLevel="h4" className="kuadrant-dashboard-learning">
+                    <GlobeIcon /> Learning Resources
+                  </Title>
+                  <p>Learn how to create, import and use {t('Kuadrant')} policies on OpenShift with step-by-step instructions and tasks.</p>
+                  <br />
+                  <Text component="a" href="/kuadrant/all-namespaces/policies" className="kuadrant-dashboard-resource-link">
+                    Create Policies in {t('Kuadrant')} <ArrowRightIcon />
+                  </Text>
+                  <br />
+                  <Text component="a" href={`/k8s/ns/${activeNamespace}/gateway.networking.k8s.io~v1~Gateway/~new`} className="kuadrant-dashboard-resource-link"                   >
+                    Add a new Gateway <ArrowRightIcon />
+                  </Text>
+                </FlexItem>
+                <Divider orientation={{ default: 'vertical' }} />
+                <FlexItem flex={{ default: 'flex_1' }}>
+                  <Title headingLevel="h4" className="kuadrant-dashboard-feature-highlights">
+                    <OptimizeIcon /> Feature Highlights
+                  </Title>
+                  <p>Read about the latest information and key features in the {t('Kuadrant')} highlights.</p>
+                </FlexItem>
+                <Divider orientation={{ default: 'vertical' }} />
+                <FlexItem flex={{ default: 'flex_1' }}>
+                  <Title headingLevel="h4" className="kuadrant-dashboard-enhance">
+                    <ReplicatorIcon /> Enhance Your Work
+                  </Title>
+                  <p>Ease operational complexity with API management and App Connectivity by using additional Operators and tools.</p>
+                </FlexItem>
+              </Flex>
+            </CardBody>
+          </Card>
         </PageSection>
-
       </Page>
     </>
   );
